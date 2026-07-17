11c108e6891bbfbbbbfc8b38db71c67d0558bf70
tasrifeahmed9-boop <tasrifeahmed9@gmail.com>
2026-07-17 19:29:24 +0600

    Update cartoons.js

--- cartoons.js
+++ cartoons.js
@@ -1,1 +1,37 @@
+import { db } from "./firebase.js";
+
+import {
+collection,
+getDocs,
+query,
+orderBy
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const cartoonCollection = collection(db, "cartoons");
+
+async function loadCartoons() {
+
+try {
+
+const q = query(cartoonCollection, orderBy("createdAt", "desc"));
+
+const snapshot = await getDocs(q);
+
+console.log("Cartoons Loaded:", snapshot.size);
+
+snapshot.forEach((doc) => {
+
+console.log(doc.id, doc.data());
+
+});
+
+} catch (err) {
+
+console.error(err);
+
+}
+
+}
+
+loadCartoons();
 
